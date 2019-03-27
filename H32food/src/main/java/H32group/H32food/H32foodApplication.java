642a412a81dcec25bf0dd3dc0396289873162e91
nguyenvukhanhhuy <khanhhuynguyenvu@gmail.com>
2019-03-27 19:29:34 +0700

    #1 first commit

---
+++ H32food/src/main/java/H32group/H32food/H32foodApplication.java
@@ -0,0 +1,13 @@
+package H32group.H32food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H32foodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H32foodApplication.class, args);
+	}
+
+}
